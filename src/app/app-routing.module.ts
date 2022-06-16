c730263ece56211ddf52571f7e83b1fdd9845b73
Ramon Rivera <ramonfrv17@gmail.com>
2022-06-16 15:29:51 -0700

    Se arreglo el navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), NgbPaginationModule, NgbAlertModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root, ngbd-collapse-navbar',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   
 })
 export class AppComponent {
   title = 'Portafolio';
-}
+  public isMenuCollapsed = true;
 
+}
